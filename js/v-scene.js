dfd85a336b87d2cdb9ca1dde5812c979f6d5f485
yijingw37 <31359897+yijingw37@users.noreply.github.com>
2022-05-29 20:04:14 -0500

    added earth texture

--- js/v-scene.js
+++ js/v-scene.js
@@ -22,8 +22,8 @@ Vue.component("room-scene", {
 		
 		<!--------- ASSETS ---------->
 		<a-assets>
-			<img id="sky" src="img/textures/sky-night.png">
-			<img id="earth" src="img/textures/earth.jpg">
+			<img id="sky" crossOrigin="anonymous" src="img/textures/sky-night.png">
+			<img id="earth" crossOrigin="anonymous" src="https://cdn.glitch.global/2a3c8a33-ddcb-41e4-9d0c-e43c923a4c1b/pinkStrip.png?v=1651179624008">
 		</a-assets>
 
 		<!--------- CAMERA --------->
